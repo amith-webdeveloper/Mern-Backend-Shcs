75bbc578d0bb9a899455325fbd1cf963d81eb819
amith-webdeveloper <amithwebdev1840@gmail.com>
2024-07-28 21:44:46 +0530

    form handling session and cookie

--- day04/index.js
+++ day04/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
 app.use(function(req , res , next){
     console.log("middleware is started..");
     next() 
