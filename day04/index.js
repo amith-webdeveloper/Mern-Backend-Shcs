09399122751238262f3391901c970b676b59e5ee
amith-webdeveloper <amithwebdev1840@gmail.com>
2024-07-28 21:14:27 +0530

    express.js routing  and middlewares

---
+++ day04/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+const app = express()
+
+app.use(function(req , res , next){
+    console.log("middleware is started..");
+    next() 
+})
+
+app.use(function(req , res , next){
+    console.log("middleware is started 2nd time..");
+    next()
+    
+})
+
+app.get('/' , (req , res) => {
+    res.send('Hello world')
+})
+
+
+
+app.get('/profile' , (req , res , next) => {
+    return next(new Error("Something Went Wrong"))
+    
+})
+
+app.use(function(err , req , res , next){
+    console.error(err.stack);
+    res.status(500).send("Something Broke!")
+    
+})
+
+app.listen(3000 , function(){
+    console.log("app is listening...");
+    
+})
+
