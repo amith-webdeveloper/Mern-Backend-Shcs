ed8ff4b40c16a181abd909404e43dd667c599077
amith-webdeveloper <amithwebdev1840@gmail.com>
2024-07-30 20:32:10 +0530

    adding edit feature in ejs backend project

--- day07/index.js
+++ day07/index.js
@@ -22,6 +22,16 @@ app.get('/file/:filename' , function(req , res){
     })
 })
 
+app.get('/edit/:filename' , function(req , res){
+    res.render('edit' , {filename: req.params.filename})
+})
+
+app.post('/edit' , function(req , res){
+    fs.rename(`./files/${req.body.previous}` , `./files/${req.body.new}` , function(err){
+        res.redirect("/")
+    })
+})
+
 app.post('/create' , function(req , res){
     // console.log(req.body);
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt` , req.body.details , function(err){
